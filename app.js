998b63388d162624dac13274d74d7dd60eeae03f
Demelza <demelzasantamariagil@gmail.com>
2022-11-07 17:45:06 +0100

    New folder Small changes in JS CSS and HTML Changed Bootstrap template

--- app.js
+++ app.js
@@ -47,14 +47,14 @@ class Dish {
 }
 
 let dishes = [];//Array of dishes
-dishes[0] = new Dish("Plato 1", 10, "Plato delicioso", [], "Iconos/vegano.png");
-dishes[1] = new Dish("Plato 2", 9, "Plato delicioso", [], "Iconos/vegano.png");
-dishes[2] = new Dish("Plato 3", 8, "Plato delicioso", [], "Iconos/vegano.png");
-dishes[3] = new Dish("Plato 4", 7, "Plato delicioso", [], "Iconos/vegano.png");
-dishes[4] = new Dish("Plato 5", 6, "Plato delicioso", [], "Iconos/vegano.png");
+dishes[0] = new Dish("Plato 1", 10, "Plato delicioso", [], "Platos/Albondigas.jpg");
+dishes[1] = new Dish("Plato 2", 9, "Plato delicioso", [], "Platos/Lasaña.jpg");
+dishes[2] = new Dish("Plato 3", 8, "Plato delicioso", [], "Platos/Lentejas.jpeg");
+dishes[3] = new Dish("Plato 4", 7, "Plato delicioso", [], "Platos/Pesto.jpg");
+dishes[4] = new Dish("Plato 5", 6, "Plato delicioso", [], "Platos/Pizza.jpg");
 dishes[5] = new Dish("Plato 6", 5, "Plato delicioso", [], "Iconos/vegano.png");
 
-page=1;
+page = 0;
 showDishes(dishes, page);
 
 
@@ -76,7 +76,7 @@ function showDishes(dishes, page) {
         }
         else {
             let img = document.getElementById("Image" + i);//Refresh Image
-            img.innerHTML = '<img src ="Iconos/Plato.png" class="img-responsive" width="100%" height="100%"></img>'; //Prints a default image when there are no dishes
+            img.innerHTML = '<img src ="Platos/Plato.png" class="img-responsive" width="100%" height="100%"></img>'; //Prints a default image when there are no dishes
 
             let name = document.getElementById("Name" + i);//Refresh Name
             name.innerHTML = '<p>No hay plato</p>'; //Prints a default name
@@ -85,13 +85,13 @@ function showDishes(dishes, page) {
             price.innerHTML = '<p>???$</p>';
 
             let moreInfo = document.getElementById("Info" + i);//Refresh Button
-            moreInfo.innerHTML = `<button id="Button ${i}" onclick="confirm('No implementado ${n+i}')">Mas info</button>` //newDish() //Print a button to create a new dish
+            moreInfo.innerHTML = `<button id="Button ${i}" onclick="confirm('No implementado ${n + i}')">Mas info</button>` //newDish() //Print a button to create a new dish
         }
 
     }
 }
 function showSpecificDish(i) {
-    console.log(dishes[page*4+parseInt(i)]);
+    console.log(dishes[page * 4 + parseInt(i)]);
 }
 
 function newDish() {
