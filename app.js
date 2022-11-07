11cb34d0b3b5ff8972dce0f8740be465fb81994a
Demelza <demelzasantamariagil@gmail.com>
2022-11-07 04:47:51 +0100

    Reorganized image folders Header improvements

--- app.js
+++ app.js
@@ -47,11 +47,12 @@ class Dish {
 }
 
 let dishes = [];//Array of dishes
-dishes[0] = new Dish("Plato 1", 10, "Plato delicioso", [], "Imagenes/vegano.png");
-dishes[1] = new Dish("Plato 2", 9, "Plato delicioso", [], "Imagenes/vegano.png");
-dishes[2] = new Dish("Plato 3", 8, "Plato delicioso", [], "Imagenes/vegano.png");
-dishes[3] = new Dish("Plato 4", 7, "Plato delicioso", [], "Imagenes/vegano.png");
-dishes[4] = new Dish("Plato 5", 6, "Plato delicioso", [], "Imagenes/vegano.png");
+dishes[0] = new Dish("Plato 1", 10, "Plato delicioso", [], "Iconos/vegano.png");
+dishes[1] = new Dish("Plato 2", 9, "Plato delicioso", [], "Iconos/vegano.png");
+dishes[2] = new Dish("Plato 3", 8, "Plato delicioso", [], "Iconos/vegano.png");
+dishes[3] = new Dish("Plato 4", 7, "Plato delicioso", [], "Iconos/vegano.png");
+dishes[4] = new Dish("Plato 5", 6, "Plato delicioso", [], "Iconos/vegano.png");
+dishes[5] = new Dish("Plato 6", 5, "Plato delicioso", [], "Iconos/vegano.png");
 
 page=1;
 showDishes(dishes, page);
@@ -75,7 +76,7 @@ function showDishes(dishes, page) {
         }
         else {
             let img = document.getElementById("Image" + i);//Refresh Image
-            img.innerHTML = '<img src ="imagenes/Plato.png" class="img-responsive" width="100%" height="100%"></img>'; //Prints a default image when there are no dishes
+            img.innerHTML = '<img src ="Iconos/Plato.png" class="img-responsive" width="100%" height="100%"></img>'; //Prints a default image when there are no dishes
 
             let name = document.getElementById("Name" + i);//Refresh Name
             name.innerHTML = '<p>No hay plato</p>'; //Prints a default name
