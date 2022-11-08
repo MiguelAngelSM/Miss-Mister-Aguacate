3f6a14242735b7d421f9cf19005bb68b63997d7c
MiguelAngelSM <masanchezmiranda31@gmail.com>
2022-11-08 17:10:27 +0100

    Change Mode did but with a bug: Bug changing from DrinkMode to others

--- app.js
+++ app.js
@@ -46,37 +46,39 @@ class Dish {
 
 }
 
-let dishes = [];//Array of dishes
-dishes[0] = new Dish("Albondigas", 10, "Plato delicioso", [], "Platos/Albondigas.jpg");
-dishes[1] = new Dish("Lasaña", 9, "Plato delicioso", [], "Platos/Lasaña.jpg");
-dishes[2] = new Dish("Lentejas", 8, "Plato delicioso", [], "Platos/Lentejas.jpeg");
-dishes[3] = new Dish("Pollo al pesto", 7, "Plato delicioso", [], "Platos/Pesto.jpg");
-dishes[4] = new Dish("Pizza 0% vegana", 6, "Plato delicioso", [], "Platos/Pizza.jpg");
-dishes[5] = new Dish("Tallarines chinos con setas y pollo", 5, "Plato delicioso", [], "Platos/Tallarines.png"); 
+let dishes = new Map();//Array of dishes //N for normal dishes, V for vegan dishes and D for drinks 
+dishes.set('N0',new Dish("Albóndigas", 10, "Plato delicioso", [], "Platos/Albondigas.jpg"));
+dishes.set('N1',new Dish("Lasaña", 9, "Plato delicioso", [], "Platos/Lasaña.jpg"));
+dishes.set('N2',new Dish("Lentejas", 8, "Plato delicioso", [], "Platos/Lentejas.jpeg"));
+dishes.set('N3',new Dish("Pollo al pesto", 7, "Plato delicioso", [], "Platos/Pesto.jpg"));
+dishes.set('N4',new Dish("Pizza 0% vegana", 6, "Plato delicioso", [], "Platos/Pizza.jpg"));
+dishes.set('N5',new Dish("Tallarines chinos con setas y pollo", 5, "Plato delicioso", [], "Platos/Tallarines.png")); 
 
 page = 0;
-showDishes(dishes, page);
+type = 'N';
+showDishes(dishes, page, type);
 
-
-function showDishes(dishes, page) {
+function showDishes(dishes, page, type) {
     n = page * 4; //page*4 is to be placed in the elements of each page cause each page has 4 dishes
     for (i = 0; i < 4; i++) {
-        if (dishes[n + i]) {//undefined is false
+        if  (dishes.get(type + (n+i))){//undefined is false
+            let dish = dishes.get(type + (n+i));//Get the dish
+
             let img = document.getElementById("Image" + i);//Refresh Image
-            img.innerHTML = '<img src ="' + dishes[n + i].getImg() + '" class="dish-image img-responsive"></img>';
+            img.innerHTML = '<img src ="' + dish.getImg() + '" class="dish-image img-responsive"></img>';
 
             let name = document.getElementById("Name" + i);//Refresh Name
-            name.innerHTML = '<p>' + dishes[n + i].getName() + '</p>';
+            name.innerHTML = '<p>' + dish.getName() + '</p>';
 
             let price = document.getElementById("Price" + i);//Refresh Price
-            price.innerHTML = '<p>' + dishes[n + i].getPrice() + '$</p>';
+            price.innerHTML = '<p>' + dish.getPrice() + '$</p>';
 
             let moreInfo = document.getElementById("Info" + i);//Refresh Button
-            moreInfo.innerHTML = `<button id="Button${i}" onclick="showSpecificDish(${i})">Mas info</button>` //Create the button
+            moreInfo.innerHTML = `<button id="Button${i}" onclick="showSpecificDish(${dish})">Mas info</button>` //Create the button
         }
         else {
             let img = document.getElementById("Image" + i);//Refresh Image
-            img.innerHTML = '<img src ="Platos/Plato.png" class="dish-image img-responsive"></img>'; //Prints a default image when there are no dishes
+            img.innerHTML = '<img src ="Iconos/Plato.png" class="dish-image img-responsive"></img>'; //Prints a default image when there are no dishes
 
             let name = document.getElementById("Name" + i);//Refresh Name
             name.innerHTML = '<p>No hay plato</p>'; //Prints a default name
@@ -85,15 +87,63 @@ function showDishes(dishes, page) {
             price.innerHTML = '<p>???$</p>';
 
             let moreInfo = document.getElementById("Info" + i);//Refresh Button
-            moreInfo.innerHTML = `<button id="Button ${i}" onclick="confirm('No implementado ${n + i}')">Mas info</button>` //newDish() //Print a button to create a new dish
+            moreInfo.innerHTML = `<button id="Button ${i}" onclick="newDish()')">Mas info</button>` //newDish() //Print a button to create a new dish
         }
-
     }
 }
-function showSpecificDish(i) {
-    console.log(dishes[page * 4 + parseInt(i)]);
+function showSpecificDish(dish) {
+    console.log(dish);
 }
 
 function newDish() {
-    alert("No esta inplementada");
+    confirm("No esta inplementada");
+}
+
+function changeVeganMode(type){//The argument type is in what type the page was 
+    let b = document.getElementById('VeganButton');
+    if (type = 'N'){
+        b.id = 'NormalButton';
+        b.innerHTML = `<button id="NormalMode" class="MenuButton" onclick="changeNormalMode('V')"><img class="menu-header-button img-responsive" src="Iconos/Normal.png"></button>`
+        b = document.getElementById('DrinkButton');//Update Button
+        b.innerHTML = `<button id="DrinkMode" class="MenuButton" onclick="changeDrinkMode('V')"><img class="menu-header-button img-responsive" src="Iconos/Bebida.png"></button>`
+    }
+    else{
+        b.id = 'DrinkButton';
+        b.innerHTML = `<button id="DrinkMode" class="MenuButton" onclick="changeDrinkMode('V')"><img class="menu-header-button img-responsive" src="Iconos/Bebida.png"></button>`
+        b = document.getElementById('NormalButton');//Update Button
+        b.innerHTML = `<button id="NormalMode" class="MenuButton" onclick="changeNormalMode('V')"><img class="menu-header-button img-responsive" src="Iconos/Normal.png"></button>`
+    }
+    showDishes(dishes, 0, 'V');//It will start at page 0
+}
+function changeNormalMode(type){ //The argument type is in what type the page was 
+    let b = document.getElementById('NormalButton');
+    if (type = 'V'){
+        b.id = 'VeganButton';
+        b.innerHTML = `<button id="VeganMode" class="MenuButton" onclick="changeVeganMode('N')"><img class="menu-header-button img-responsive" src="Iconos/vegano.png"></button>`
+        b = document.getElementById('DrinkButton');//Update Button
+        b.innerHTML = `<button id="DrinkMode" class="MenuButton" onclick="changeDrinkMode('N')"><img class="menu-header-button img-responsive" src="Iconos/Bebida.png"></button>`
+    }
+    else{
+        b.id = 'DrinkButton';
+        b.innerHTML = `<button id="DrinkMode" class="MenuButton" onclick="changeDrinkMode('N')"><img class="menu-header-button img-responsive" src="Iconos/Bebida.png"></button>`
+        b = document.getElementById('VeganButton');//Update Button
+        b.innerHTML = `<button id="VeganMode" class="MenuButton" onclick="changeVeganMode('N')"><img class="menu-header-button img-responsive" src="Iconos/vegano.png"></button>`
+    }
+    showDishes(dishes, 0, 'N');//It will start at page 0
+}
+function changeDrinkMode(type){//The argument type is in what type the page was 
+    let b = document.getElementById('DrinkButton');
+    if (type = 'N'){
+        b.id = 'NormalButton';
+        b.innerHTML = `<button id="NormalMode" class="MenuButton" onclick="changeNormalMode('D')"><img class="menu-header-button img-responsive" src="Iconos/Normal.png"></button>`
+        b = document.getElementById('VeganButton');//Update Button
+        b.innerHTML = `<button id="VeganMode" class="MenuButton" onclick="changeVeganMode('D')"><img class="menu-header-button img-responsive" src="Iconos/vegano.png"></button>`
+    }
+    else{
+        b.id = 'VeganButton';
+        b.innerHTML = `<button id="VeganMode" class="MenuButton" onclick="changeVeganMode('D')"><img class="menu-header-button img-responsive" src="Iconos/vegano.png"></button>`
+        b = document.getElementById('NormalButton');//Update Button
+        b.innerHTML = `<button id="NormalMode" class="MenuButton" onclick="changeNormalMode('D')"><img class="menu-header-button img-responsive" src="Iconos/Normal.png"></button>`
+    }
+    showDishes(dishes, 0, 'D');//It will start at page 0
 }
