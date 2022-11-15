f1f47cbc8ef8a2ab88fe6bd653cdd26e0cd6e01a
MiguelAngelSM <masanchezmiranda31@gmail.com>
2022-11-15 23:38:35 +0100

    Modify dish implemented, create new dish fixed and default dishes info updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ class Dish {
   description;
   atributes = [];
   img; //image link
-  static amount = [0,0,0]; //Normal Vegan Drink
+  static amount = [0, 0, 0]; //Normal Vegan Drink
 
   constructor(name, price, description, atributes, img, type) {
     this.setName(name);
@@ -15,22 +15,50 @@ class Dish {
     Dish.setAmount(type);
   }
 
-  static getAmount(type){
+  static removeDish(type) {
     let n = 0;
-    switch(type){
-      case 'N': n=0;break;
-      case 'V': n=1;break;
-      case 'D': n=2;break;
+    switch (type) {
+      case "N":
+        n = 0;
+        break;
+      case "V":
+        n = 1;
+        break;
+      case "D":
+        n = 2;
+        break;
+    }
+    Dish.amount[n]--;
+  }
+
+  static getAmount(type) {
+    let n = 0;
+    switch (type) {
+      case "N":
+        n = 0;
+        break;
+      case "V":
+        n = 1;
+        break;
+      case "D":
+        n = 2;
+        break;
     }
     return Dish.amount[n];
   }
 
-  static setAmount(type){
+  static setAmount(type) {
     let n = 0;
-    switch(type){
-      case 'N': n=0;break;
-      case 'V': n=1;break;
-      case 'D': n=2;break;
+    switch (type) {
+      case "N":
+        n = 0;
+        break;
+      case "V":
+        n = 1;
+        break;
+      case "D":
+        n = 2;
+        break;
     }
     Dish.amount[n]++;
   }
@@ -65,7 +93,7 @@ class Dish {
   setImg(value) {
     this.img = value;
   }
-  addAtribute(value){
+  addAtribute(value) {
     this.atributes.push(value);
   }
 }
@@ -95,27 +123,54 @@ function createDefaultDishes(dishes) {
   dishes.set(
     "N0",
     new Dish(
-      "Albóndigas",
-      10,
-      "Plato delicioso",
-      ['a','b','c','d'],
-      "Platos/Normal/Albondigas.jpg",
-      'N'
+      "Hamburguesa completa",
+      5,
+      "Hamburguesa completa con carne mixta, lechuga, tomate, cebolla y queso. Salsas a elegir entre mayonesa, ketchup, mostaza y salsa BBQ. ¡INCLUYE PATATAS FRITAS O DELUXE!",
+      [
+        "Carne mixta de ternera y cerdo",
+        "Cebolla dulce",
+        "Lechuga iceberg",
+        "Tomate",
+        "Queso cheddar",
+        "Ketchup Heinz",
+        "Mayonesa Hacendado",
+        "Mostaza",
+        "Salsa Barbacoa",
+      ],
+      "Platos/Normal/Burguer.jpg",
+      "N"
     )
   );
   dishes.set(
     "N1",
-    new Dish("Lasaña", 9, "Plato delicioso", [], "Platos/Normal/Lasaña.jpg")
+    new Dish(
+      "Pizza",
+      8,
+      "Pizza personalizable con base de tomate y queso",
+      [
+        "4 quesos",
+        "Salsa Barbacoa",
+        "Atún",
+        "Carne picada",
+        "Bacon",
+        "Champiñones",
+        "Aceitunas negras",
+        "Salchichas",
+        "Jamon Serrano",
+        "Queso de cabra",
+      ],
+      "Platos/Normal/Pizza.jpg"
+    )
   );
   dishes.set(
     "N2",
     new Dish(
-      "Lentejas",
-      8,
-      "Plato delicioso",
-      [],
-      "Platos/Normal/Lentejas.jpeg",
-      'N'
+      "Macrrones con albondigas",
+      4,
+      "Deliciosos macarrones con tomate con sabrosas albondigas hechas al estilo de la abuela",
+      ["Macarrones", "Tomate frito natural", "Albondigas de carne mixta"],
+      "Platos/Normal/Macarrones con albondigas.jpg",
+      "N"
     )
   );
   dishes.set(
@@ -123,32 +178,63 @@ function createDefaultDishes(dishes) {
     new Dish(
       "Pollo al pesto",
       7,
-      "Plato delicioso",
-      [],
+      "Delicioso plato de pollo al pesto al puro estilo Sevillano",
+      [
+        "Macarrones helice",
+        "Pollo a la plancha",
+        "Mozzarella",
+        "Pesto",
+        "Tomates Cherry",
+      ],
       "Platos/Normal/Pesto.jpg",
-      'N'
+      "N"
     )
   );
   dishes.set(
     "N4",
     new Dish(
-      "Pizza 0% vegana",
-      6,
-      "Plato delicioso",
-      [],
-      "Platos/Normal/Pizza.jpg",
-      'N'
+      "Fajitas",
+      8,
+      "Deliciosas fajitas hechas al puro estilo mexicano. ¡INCLUYE NACHOS CON SALSAS!",
+      [
+        "Tortilla de trigo",
+        "Tiras de pollo",
+        "Pimiento verde, rojo y amarillo",
+        "Especias variadas",
+        "Nachos de trigo",
+        "Guacamole",
+        "Salsa de tomate mexicana",
+        "Salsa de queso mexicana",
+      ],
+      "Platos/Normal/Fajitas.jpg",
+      "N"
     )
   );
   dishes.set(
     "N5",
     new Dish(
       "Tallarines chinos con setas y pollo",
-      5,
-      "Plato delicioso",
-      [],
+      3,
+      "Deliciosos tallarines fritos hechos por un hombre de las tierras del Quijote",
+      ["Tallarines", "Champiñones", "Pollo a la plancha", "Salsa de soja"],
       "Platos/Normal/Tallarines.jpg",
-      'N'
+      "N"
+    )
+  );
+  dishes.set(
+    "N6",
+    new Dish(
+      "Pescado con papas",
+      4,
+      "Podría parecer que es una copia del fish&chips ingles pero no, este pescado con papas es mejor gracias a nuestro chef valenciano que pone su toque culinario",
+      [
+        "Filete de merluza a la plancha",
+        "Patatas fritas",
+        "Cebolla",
+        "Pimiento",
+      ],
+      "Platos/Normal/Pescado con papas.jpg",
+      "N"
     )
   );
   //4 Normal dishes by default
@@ -156,44 +242,77 @@ function createDefaultDishes(dishes) {
     "V0",
     new Dish(
       "Hamburguesa vegana",
-      10,
-      "Plato delicioso",
-      [],
+      5,
+      "Deliciosa hamburguesa vegana hecha para los amantes de los sabores exóticos",
+      [
+        '"Carne" Beyond',
+        "Cebolla morada",
+        "Rúcula",
+        "Aguacate",
+        "Platano macho frito",
+        "Salsa secreta de la casa",
+      ],
       "Platos/Vegano/Burguer Vegana.jpg",
-      'V'
+      "V"
     )
   );
   dishes.set(
     "V1",
     new Dish(
       "Ensalada vegana",
-      9,
-      "Plato delicioso",
-      [],
+      3,
+      "Plato vegano por defecto en cualquier restaurante",
+      [
+        "Lechuga iceberg",
+        "Cebolla",
+        "Tomate",
+        "Sal",
+        "Aceite",
+        "Vinagre",
+        "Pepino",
+        "Rúcula",
+      ],
       "Platos/Vegano/Ensalada Vegana.jpg",
-      'V'
+      "V"
     )
   );
   dishes.set(
     "V2",
     new Dish(
-      "Macarrones con tomatico y queso",
-      8,
-      "Plato delicioso",
-      [],
+      "Macarrones con tomate y queso",
+      4,
+      "Macarrones con tomate y queso 100% vegano",
+      ["Macarrones", "Tomate frito natural", "Queso vegano"],
       "Platos/Vegano/Pasta Vegana.jpg",
-      'V'
+      "V"
     )
   );
   dishes.set(
     "V3",
     new Dish(
       "Tarta de Manzana",
-      7,
-      "Plato delicioso",
-      [],
+      2,
+      "Dulce postre elaborado por nuestra chef mostoleña UwU",
+      ["Manzanas"],
       "Platos/Vegano/Tarta.jpg",
-      'V'
+      "V"
+    )
+  );
+  dishes.set(
+    "V4",
+    new Dish(
+      "Pizza vegana",
+      8,
+      "Deliciosa pizza vegana para los amantes de la naturaleza",
+      [
+        "Champiñones",
+        "Base de tomate y queso vegano",
+        "Maiz",
+        "Queso vegano en rodajas",
+        "Albahaca",
+      ],
+      "Platos/Vegano/Pizza vegana.jpg",
+      "V"
     )
   );
   //6 Drinks by default
@@ -203,9 +322,9 @@ function createDefaultDishes(dishes) {
       "Agua",
       2,
       "Agua fresca de Solan de Cabras edicion Rocas del Manantial en botella de vidrio de 70cl",
-      [],
+      ["Agua de manantial"],
       "Platos/Bebida/Agua.jpg",
-      'D'
+      "D"
     )
   );
   dishes.set(
@@ -214,9 +333,9 @@ function createDefaultDishes(dishes) {
       "Cerveza",
       2,
       "Cerveza Mahou 5 estrellas en botellin de 33cl",
-      [],
+      ["Agua", "Malta de cebada", "Maíz", "Lúpulo"],
       "Platos/Bebida/Cerveza.jpg",
-      'D'
+      "D"
     )
   );
   dishes.set(
@@ -225,9 +344,18 @@ function createDefaultDishes(dishes) {
       "Refrescos",
       3,
       "Coca-Cola Normal o Zero o Light, Fanta de Naranja o de Limon, Aquarius de Naranja o de Limon, Trina de Naranja,",
-      [],
+      [
+        "Coca-Cola Normal",
+        "Coca-Cola Zero",
+        "Coca-Cola Light",
+        "Fanta de Naranja",
+        "Fanta de Limon",
+        "Aquarius de Naranja",
+        "Aquarius de Limón",
+        "Trina de Naranja",
+      ],
       "Platos/Bebida/Refrescos.jpg",
-      'D'
+      "D"
     )
   );
   dishes.set(
@@ -236,9 +364,9 @@ function createDefaultDishes(dishes) {
       "Café",
       1,
       "Café 100% natural hecho en cafetera moka italiana",
-      [],
+      ["Café de cuba", "Azucar", "Leche", "Agua"],
       "Platos/Bebida/Cafe.jpg",
-      'D'
+      "D"
     )
   );
   dishes.set(
@@ -247,9 +375,9 @@ function createDefaultDishes(dishes) {
       "Vino Pasion de Bobal",
       7.95,
       "Vino ecologico con origen en viñedos de más de 60 años ",
-      [],
+      ["Agua", "Alcohol", "Glucosa y fructosa"],
       "Platos/Bebida/VinoBarato.jpg",
-      'D'
+      "D"
     )
   );
   dishes.set(
@@ -258,12 +386,11 @@ function createDefaultDishes(dishes) {
       "Vino Teso La Monja",
       1272,
       "Vino de gran calidad cultivado segun los principios de la biodinámica",
-      [],
+      ["Agua", "Alcohol", "Glucosa y fructosa"],
       "Platos/Bebida/VinoCaro.jpg",
-      'D'
+      "D"
     )
   );
-  //Add Normal Burguer Vegan Pizza Remove Lentejas
 }
 
 function changePage(mode, page, nextOrPrevious) {
@@ -272,7 +399,9 @@ function changePage(mode, page, nextOrPrevious) {
   //only change page if it is a valid change(if you are in the first page you can't go to the previous page and if there are more
   //dishes to show or there aren't any 'Añadir Plato' button)
   if (
-    (nextOrPrevious === 1 && page >= 0 && page + 1 <= Dish.getAmount(mode) / 4) ||
+    (nextOrPrevious === 1 &&
+      page >= 0 &&
+      page + 1 <= Dish.getAmount(mode) / 4) ||
     (nextOrPrevious === -1 && page !== 0)
   ) {
     p = page + nextOrPrevious;
@@ -321,7 +450,9 @@ function showDishes(dishes, page, mode) {
       price.innerHTML = "<p>" + dish.getPrice() + "$</p>";
 
       let moreInfo = document.getElementById("Info" + i); //Refresh Button
-      moreInfo.innerHTML = `<button id="Button${i}" onclick="showSpecificDish('${mode + (n + i)}')">Mas info</button>`; //Create the button
+      moreInfo.innerHTML = `<button id="Button${i}" onclick="modifyDish('${
+        mode + (n + i)
+      }')">Modificar</button>`; //Create the button
     } else {
       let img = document.getElementById("Image" + i); //Refresh Image
       img.innerHTML =
@@ -354,97 +485,151 @@ function updateArrows(mode, page) {
 
 //Here starts the forms functions
 
-function showIngredientsList(key){
+function showIngredientsList(key) {
   //Delete the previous list and make it again with all the elements from the list got from the dish got from the key
-  dish=dishes.get(key);
-  ingredients=dish.getAtributes();
-  e=document.getElementById('IngredientsList');
-  e.innerHTML=``;
-  ingredients.forEach(i=>printIngredient(i,key));
+  dish = dishes.get(key);
+  ingredients = dish.getAtributes();
+  e = document.getElementById("IngredientsList");
+  e.innerHTML = ``;
+  ingredients.forEach((i) => printIngredient(i, key));
 }
 
-function printIngredient(ingredient,key){
+function printIngredient(ingredient, key) {
   //Add to the IngredientsList the ingredient
   //The key is required for the delete button
-  e=document.getElementById('IngredientsList');
-  e.innerHTML+=`<div>
+  e = document.getElementById("IngredientsList");
+  e.innerHTML += `<div>
     ${ingredient} 
     <button onclick="deleteIngredient('${ingredient}','${key}')">Eliminar</button>
-  <div>`
+  <div>`;
 }
 
-function deleteIngredient(ingredient,key){
+function deleteIngredient(ingredient, key) {
   //Delete the ingredient from the dish got from the key and then update the list
-  dish=dishes.get(key);
-  list=dish.getAtributes();
-  list.splice(list.indexOf(ingredient),1);
+  dish = dishes.get(key);
+  list = dish.getAtributes();
+  list.splice(list.indexOf(ingredient), 1);
   showIngredientsList(key);
 }
 
-function newDish(mode){
+function newDish(mode) {
   //Hide the Menu and show the form
-  //Create a default dish that will be edited or deleted 
+  //Create a default dish that will be edited or deleted
   //Update the page buttons
-  e = document.getElementById('Menu');
-  e.style.display = 'none';
-  e = document.getElementById('Form');
-  e.style.display = 'block';
+  e = document.getElementById("Menu");
+  e.style.display = "none";
+  e = document.getElementById("Form");
+  e.style.display = "block";
 
   dishes.set(
     mode + Dish.getAmount(mode),
     new Dish(
-      'Default dish',
-      '???',
-      'This dish was generated because the form was empty',
+      "Default dish",
+      "???",
+      "This dish was generated because the form was empty",
       [],
       "Iconos/Plato.png",
       mode
     )
   );
-  showIngredientsList(mode + (Dish.getAmount(mode)-1));
-  
-  e = document.getElementById('IngredientButton');
-  e.onclick= function (){addNewIngredient(mode + (Dish.getAmount(mode)-1));};
-  e = document.getElementById('formSaveButton');
-  e.onclick=function (){saveNewDish(mode + (Dish.getAmount(mode)-1));};
-  e = document.getElementById('formCancelButton');
-  e.onclick=function (){backButton(mode);};
+  showIngredientsList(mode + (Dish.getAmount(mode) - 1));
+
+  key = mode + (Dish.getAmount(mode) - 1);
+  e = document.getElementById("IngredientButton");
+  e.onclick = function () {
+    addNewIngredient(key);
+  };
+  e = document.getElementById("formSaveButton");
+  e.onclick = function () {
+    saveNewDish(key, 1);
+  };
+  e = document.getElementById("formCancelButton");
+  e.onclick = function () {
+    backButton(key, 1);
+  };
 }
 
-function backButton(mode){
+function backButton(key, opt, copyArray) {
   //Delete the dish and return to the Menu
-  dishes.delete(mode + (Dish.getAmount(mode)-1));
-  backToMenu();
-}
-
-function backToMenu(){
-  //Hide the Form and return to the normal mode page 0 (default page)
-  e = document.getElementById('Menu');
-  e.style.display = 'block';
-  e = document.getElementById('Form');
-  e.style.display = 'none';
-  showDishes(dishes, 0, 'N');
-  updateArrows('N', 0);
-}
-
-function saveNewDish(key){
-  //If the user confirm at the pop-up the dish will be updated with the info from the form
-  //If the user cancel at the pop-up the dish will be deleted from the map
-  //The it will back to menu
-  if (confirm('Deseas guardar el plato?')){
-    dishes.get(key).setName(document.getElementById('Name').value);
-    dishes.get(key).setPrice(document.getElementById('Price').value);
-    dishes.get(key).setDescription(document.getElementById('Description').value);
-  }else{
+  //Option 1 delete the last dish, any other value won't delete it
+  if (opt === 1) {
     dishes.delete(key);
+    Dish.removeDish(key[0]);
+  } else {
+    dishes.get(key).setAtributes(copyArray);
   }
   backToMenu();
 }
 
-function addNewIngredient(key){
+function backToMenu() {
+  //Hide the Form and return to the normal mode page 0 (default page)
+  e = document.getElementById("Menu");
+  e.style.display = "block";
+  showDishes(dishes, 0, "N");
+  updateArrows("N", 0);
+  changeMenuButtons("Normal", "Vegan", "Drink");
+
+  document.getElementById("Name").value = "";
+  document.getElementById("Price").value = "";
+  document.getElementById("Description").value = "";
+  document.getElementById("Ingredient").value = "";
+  e = document.getElementById("Form");
+  e.style.display = "none";
+}
+
+function saveNewDish(key, opt, copyArray) {
+  //If the user confirm at the pop-up the dish will be updated with the info from the form
+  //If the user cancel at the pop-up the dish will be deleted from the map
+  //It will clean the form also
+  //The it will back to menu
+  if (confirm("Deseas guardar el plato?")) {
+    dishes.get(key).setName(document.getElementById("Name").value);
+    dishes.get(key).setPrice(document.getElementById("Price").value);
+    dishes
+      .get(key)
+      .setDescription(document.getElementById("Description").value);
+  } else {
+    if (opt === 1) {
+      dishes.delete(key);
+      Dish.removeDish(key[0]); //First char from the array is the mode
+    } else {
+      dishes.get(key).setAtributes(copyArray);
+    }
+  }
+  backToMenu();
+}
+
+function addNewIngredient(key) {
   //Add an ingredient to the dish got from the key and refresh the list
-  dishes.get(key).addAtribute(document.getElementById('Ingredient').value);
+  dishes.get(key).addAtribute(document.getElementById("Ingredient").value);
   showIngredientsList(key);
 }
 
-//El problema es que al guardar un plato ejecuta dos veces la funcion saveNewDish(430) ligada al boton del div de formSaveButton (287 del html)
+function modifyDish(key) {
+  e = document.getElementById("Menu");
+  e.style.display = "none";
+  e = document.getElementById("Form");
+  e.style.display = "block";
+
+  document.getElementById("Name").value = dishes.get(key).getName();
+  document.getElementById("Price").value = dishes.get(key).getPrice();
+  document.getElementById("Description").value = dishes
+    .get(key)
+    .getDescription();
+  showIngredientsList(key);
+
+  let copyArray = [...dishes.get(key).getAtributes()];
+
+  e = document.getElementById("IngredientButton");
+  e.onclick = function () {
+    addNewIngredient(key);
+  };
+  e = document.getElementById("formSaveButton");
+  e.onclick = function () {
+    saveNewDish(key, 0, copyArray);
+  };
+  e = document.getElementById("formCancelButton");
+  e.onclick = function () {
+    backButton(key, 0, copyArray);
+  };
+}
