8bdf4054b08c1d847a7200af32e98be92ad9d6cf
Demelza <demelzasantamariagil@gmail.com>
2022-11-18 02:00:42 +0100

    New Footer and small changes in style

--- Modules/dish.js
+++ Modules/dish.js
@@ -99,7 +99,7 @@ export class Dish {
   }
 }
 
-export let dishes = new Map(); //Map of dishes 
+export let dishes = new Map(); //Map of dishes
 //The keys will be made by a letter (N for normal dishes, V for vegan dishes and D for drinks) and a number
 
 //Deafult dishes definition
@@ -107,15 +107,15 @@ dishes.set(
   "N0",
   new Dish(
     "Hamburguesa completa",
-    5,
-    "Hamburguesa completa con carne mixta, lechuga, tomate, cebolla y queso. Salsas a elegir entre mayonesa, ketchup, mostaza y salsa BBQ. ¡INCLUYE PATATAS FRITAS O DELUXE!",
+    "5€",
+    "Hamburguesa completa con carne mixta, lechuga, tomate, cebolla y queso. Salsas a elegir entre mayonesa, kétchup, mostaza y salsa BBQ. ¡INCLUYE PATATAS FRITAS O DELUXE!",
     [
       "Carne mixta de ternera y cerdo",
       "Cebolla dulce",
       "Lechuga iceberg",
       "Tomate",
       "Queso cheddar",
-      "Ketchup Heinz",
+      "Kétchup Heinz",
       "Mayonesa Hacendado",
       "Mostaza",
       "Salsa Barbacoa",
@@ -128,7 +128,7 @@ dishes.set(
   "N1",
   new Dish(
     "Pizza",
-    8,
+    "8€",
     "Pizza personalizable con base de tomate y queso",
     [
       "4 quesos",
@@ -148,10 +148,10 @@ dishes.set(
 dishes.set(
   "N2",
   new Dish(
-    "Macrrones con albondigas",
-    4,
-    "Deliciosos macarrones con tomate con sabrosas albondigas hechas al estilo de la abuela",
-    ["Macarrones", "Tomate frito natural", "Albondigas de carne mixta"],
+    "Macarrones con albóndigas",
+    "4€",
+    "Deliciosos macarrones con tomate con sabrosas albóndigas hechas al estilo de la abuela",
+    ["Macarrones", "Tomate frito natural", "Albóndigas de carne mixta"],
     "Platos/Normal/Macarrones con albondigas.jpg",
     "N"
   )
@@ -160,10 +160,10 @@ dishes.set(
   "N3",
   new Dish(
     "Pollo al pesto",
-    7,
-    "Delicioso plato de pollo al pesto al puro estilo Sevillano",
+    "7€",
+    "Delicioso plato de pollo al pesto al puro estilo sevillano",
     [
-      "Macarrones helice",
+      "Macarrones hélice",
       "Pollo a la plancha",
       "Mozzarella",
       "Pesto",
@@ -177,7 +177,7 @@ dishes.set(
   "N4",
   new Dish(
     "Fajitas",
-    8,
+    "8€",
     "Deliciosas fajitas hechas al puro estilo mexicano. ¡INCLUYE NACHOS CON SALSAS!",
     [
       "Tortilla de trigo",
@@ -197,7 +197,7 @@ dishes.set(
   "N5",
   new Dish(
     "Tallarines chinos con setas y pollo",
-    3,
+    "3€",
     "Deliciosos tallarines fritos hechos por un hombre de las tierras del Quijote",
     ["Tallarines", "Champiñones", "Pollo a la plancha", "Salsa de soja"],
     "Platos/Normal/Tallarines.jpg",
@@ -208,8 +208,8 @@ dishes.set(
   "N6",
   new Dish(
     "Pescado con papas",
-    4,
-    "Podría parecer que es una copia del fish&chips ingles pero no, este pescado con papas es mejor gracias a nuestro chef valenciano que pone su toque culinario",
+    "4€",
+    "Podría parecer que es una copia del fish&chips inglés, pero no, este pescado con papas es mejor gracias a nuestro chef valenciano que pone su toque culinario",
     ["Filete de merluza a la plancha", "Patatas fritas", "Cebolla", "Pimiento"],
     "Platos/Normal/Pescado con papas.jpg",
     "N"
@@ -220,14 +220,14 @@ dishes.set(
   "V0",
   new Dish(
     "Hamburguesa vegana",
-    5,
+    "5€",
     "Deliciosa hamburguesa vegana hecha para los amantes de los sabores exóticos",
     [
       '"Carne" Beyond',
       "Cebolla morada",
       "Rúcula",
       "Aguacate",
-      "Platano macho frito",
+      "Plátano macho frito",
       "Salsa secreta de la casa",
     ],
     "Platos/Vegano/Burguer Vegana.jpg",
@@ -238,7 +238,7 @@ dishes.set(
   "V1",
   new Dish(
     "Ensalada vegana",
-    3,
+    "3€",
     "Plato vegano por defecto en cualquier restaurante",
     [
       "Lechuga iceberg",
@@ -258,7 +258,7 @@ dishes.set(
   "V2",
   new Dish(
     "Macarrones con tomate y queso",
-    4,
+    "4€",
     "Macarrones con tomate y queso 100% vegano",
     ["Macarrones", "Tomate frito natural", "Queso vegano"],
     "Platos/Vegano/Pasta Vegana.jpg",
@@ -269,8 +269,8 @@ dishes.set(
   "V3",
   new Dish(
     "Tarta de Manzana",
-    2,
-    "Dulce postre elaborado por nuestra chef mostoleña UwU",
+    "2€",
+    "Dulce postre elaborado por nuestra chef mostoleña",
     ["Manzanas"],
     "Platos/Vegano/Tarta.jpg",
     "V"
@@ -280,12 +280,12 @@ dishes.set(
   "V4",
   new Dish(
     "Pizza vegana",
-    8,
+    "8€",
     "Deliciosa pizza vegana para los amantes de la naturaleza",
     [
       "Champiñones",
       "Base de tomate y queso vegano",
-      "Maiz",
+      "Maíz",
       "Queso vegano en rodajas",
       "Albahaca",
     ],
@@ -298,8 +298,8 @@ dishes.set(
   "D0",
   new Dish(
     "Agua",
-    2,
-    "Agua fresca de Solan de Cabras edicion Rocas del Manantial en botella de vidrio de 70cl",
+    "2€",
+    "Agua fresca de Solan de Cabras edición Rocas del Manantial en botella de vidrio de 70cl",
     ["Agua de manantial"],
     "Platos/Bebida/Agua.jpg",
     "D"
@@ -309,8 +309,8 @@ dishes.set(
   "D1",
   new Dish(
     "Cerveza",
-    2,
-    "Cerveza Mahou 5 estrellas en botellin de 33cl",
+    "2€",
+    "Cerveza Mahou 5 estrellas en botellín de 33cl",
     ["Agua", "Malta de cebada", "Maíz", "Lúpulo"],
     "Platos/Bebida/Cerveza.jpg",
     "D"
@@ -320,14 +320,14 @@ dishes.set(
   "D2",
   new Dish(
     "Refrescos",
-    3,
-    "Coca-Cola Normal o Zero o Light, Fanta de Naranja o de Limon, Aquarius de Naranja o de Limon, Trina de Naranja,",
+    "3€",
+    "Coca-Cola Normal o Zero o Light, Fanta de Naranja o de Limón, Aquarius de Naranja o de Limón, Trina de Naranja,",
     [
       "Coca-Cola Normal",
       "Coca-Cola Zero",
       "Coca-Cola Light",
       "Fanta de Naranja",
-      "Fanta de Limon",
+      "Fanta de Limón",
       "Aquarius de Naranja",
       "Aquarius de Limón",
       "Trina de Naranja",
@@ -340,9 +340,9 @@ dishes.set(
   "D3",
   new Dish(
     "Café",
-    1,
+    "1€",
     "Café 100% natural hecho en cafetera moka italiana",
-    ["Café de cuba", "Azucar", "Leche", "Agua"],
+    ["Café de Cuba", "Azúcar", "Leche", "Agua"],
     "Platos/Bebida/Cafe.jpg",
     "D"
   )
@@ -350,9 +350,9 @@ dishes.set(
 dishes.set(
   "D4",
   new Dish(
-    "Vino Pasion de Bobal",
-    7.95,
-    "Vino ecologico con origen en viñedos de más de 60 años ",
+    "Vino Pasión de Bobal",
+    "7.95€",
+    "Vino ecológico con origen en viñedos de más de 60 años ",
     ["Agua", "Alcohol", "Glucosa y fructosa"],
     "Platos/Bebida/VinoBarato.jpg",
     "D"
@@ -362,8 +362,8 @@ dishes.set(
   "D5",
   new Dish(
     "Vino Teso La Monja",
-    1272,
-    "Vino de gran calidad cultivado segun los principios de la biodinámica",
+    "1272€",
+    "Vino de gran calidad cultivado según los principios de la biodinámica",
     ["Agua", "Alcohol", "Glucosa y fructosa"],
     "Platos/Bebida/VinoCaro.jpg",
     "D"
